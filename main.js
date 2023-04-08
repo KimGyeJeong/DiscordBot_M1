f49471254c06fdf27eb0eba4c94b33b1868f39f1
kimgyejeong <pmwkdo@gmail.com>
2023-04-08 09:21:31 +0900

    start setting

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })	// 
 const dotenv = require('dotenv').config();	// .env 파일 호출
 const BOT_TOKEN = dotenv.parsed.BOT_TOKEN;	// .env 파일에서 BOT_TOKEN 변수 호출
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login(BOT_TOKEN);
