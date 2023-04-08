a152d8f966934783ebdf5a6fada9c0d9d47fadfd
kimgyejeong <pmwkdo@gmail.com>
2023-04-08 09:16:16 +0900

    start setting

---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');	// discord.js 라이브러리 호출
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })	// Client 객체 생성
+
+const dotenv = require('dotenv').config();	// .env 파일 호출
+const BOT_TOKEN = dotenv.parsed.BOT_TOKEN;	// .env 파일에서 BOT_TOKEN 변수 호출
+
